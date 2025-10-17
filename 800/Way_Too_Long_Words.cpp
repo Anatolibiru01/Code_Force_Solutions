0c0ad61f41f165c372e63358ef49beec6393ded9
mr.Toli <anatolibiru888@gmail.com>
2025-10-17 23:11:44 +0300

    800 problem which work based on length of the words

---
+++ 800/Way_Too_Long_Words.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    string word;
+    cin >> n;
+    while (n--)
+    {
+        cin >> word;
+        if (word.length() > 10)
+            cout << word[0] << word.length() - 2 << word.back() << endl;
+        else
+            cout << word << endl;
+    }
+    return 0;
+}
